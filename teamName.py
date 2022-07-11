e5b29dbdbfe94edc4278914e048d765d1f9d1162
n-dai <nealdai1@gmail.com>
2022-07-11 23:17:33 +1000

    first commit

--- teamName.py
+++ teamName.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 nInst=100
 currentPos = np.zeros(nInst)
@@ -8,6 +9,21 @@ def getMyPosition (prcSoFar):
 
     # Build your function body here
 
-    return currentPos
+    # if (prcSoFar == "hist"):
+    #     pass
+
+
+    # with open("C:/Users/Neal Dai/Documents/PYTHON_SNAKE/algothon/algothon2022-starter-code/prices.txt") as f:
+    #     lines = f.read().rstrip()
+
+    if prcSoFar == 1:
+        df = pd.read_csv("C:/Users/Neal Dai/Documents/PYTHON_SNAKE/algothon/algothon2022-starter-code/prices.txt", nrows=2)
+        print(df)
+
+    #currentPos = np.full(100, 120)
 
     
+
+    return currentPos
+
+getMyPosition(1)
